78442165377ba3a03a32de8aaed1a25361d8e806
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-03-16 00:13:16 +0000

    Update index.tsx

--- components/create/index.tsx
+++ components/create/index.tsx
@@ -114,18 +114,18 @@ const OneContent = styled.div`
 `;
 
 function CreateRaffle() {
-  const [screen, setScreen] = useState(false);screen
+  const [screen, setScreen] = useState(false);
   const [name, setName] = useState("");
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
   const [winners, setWinners] = useState("");
   const [description, setDescription] = useState("");
   const arr = [name, start, end, winners, description];
-  
+
   async function Agrs() {
     localStorage.arr = await JSON.stringify(arr);
   }
-  
+
   async function Initialize() {
     const FairContract = "0xa1eB5e2893442B0Eeb115eE0B31470790EE9D1a7";
     const ethereum = (window as any).ethereum;
