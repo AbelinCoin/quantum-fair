580d73b618ed39e5724a53170f713f2af2c71314
jose martinez <che.manuel758@gmail.com>
2023-05-10 16:03:48 -0400

    added tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { DAppProvider } from "@usedapp/core";
+import './global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DAppProvider config={{}}>
       <Component {...pageProps} />
+      <div className="flex justify-center items-center font-semibold text-4xl bg-white border border-gray-300 shadow-lg mx-6 my-12 p-4 rounded-lg py-12">Tailwind Test</div>
     </DAppProvider>
   );
 }
