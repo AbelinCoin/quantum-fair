d473b48e95fd51d1173bf69b85344d5495977f8f
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-21 06:43:52 +0000

    Update 2 files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { DAppProvider } from "@usedapp/core";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../components/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DAppProvider config={{}}>
+      <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      </ChakraProvider>
     </DAppProvider>
   );
 }
